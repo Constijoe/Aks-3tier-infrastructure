ce924e727df27076fd611d8d9a7b76790310816a
constijoe <constijoe444@gmail.com>
2026-09-20 04:17:35 +0100

    node version

--- module/aks/node-group.tf
+++ module/aks/node-group.tf
@@ -1,12 +1,11 @@
 resource "azurerm_kubernetes_cluster_node_pool" "example" {
   name                  = "internal"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = "Standard_DS2_v2"
-
-  auto_scaling_enabled = true
-  node_count           = 2
-  min_count            = 1
-  max_count            = 10
+  vm_size               = "Standard_D2_v3"
+  auto_scaling_enabled  = true
+  node_count            = 2
+  min_count             = 1
+  max_count             = 10
 
   tags = {
     Environment = var.env
